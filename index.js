346efdbd929dfdff6a0dca5e3808be3841777461
Bikash Sahoo <Yashharkawat2000@gmail.com>
2023-07-04 13:21:33 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,16 @@
+function add(num1,num2){
+    return num1+num2;
+}
+console.log("hello");
+
+let item=document.getElementsByClassName('industries_served');
+
+var element=document.createElement('img');
+
+const fs = require(['fs']);
+
+fs.readdir('./industries_images', (err, files) => {
+  if (err) throw err;
+  console.log(files);
+});
+
