d6ffd4e0e23b4c9fb102f01374c98260db752dc2
Bikash Sahoo <Yashharkawat2000@gmail.com>
2023-07-06 15:28:45 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 
-document.querySelectorAll('.items').forEach((item)=>{
-  item.addEventListener('click',()=>{
-    item.style.color='red';
-  })
-})
 console.log('hello');
 
 function add(num1,num2){
@@ -11,14 +6,4 @@ function add(num1,num2){
 }
 console.log("hello");
 
-let item=document.getElementsByClassName('industries_served');
-
-var element=document.createElement('img');
-
-const fs = require(['fs']);
-
-fs.readdir('./industries_images', (err, files) => {
-  if (err) throw err;
-  console.log(files);
-});
 
